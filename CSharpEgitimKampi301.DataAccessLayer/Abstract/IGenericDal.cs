a8af674d36ff07a3efedcddcee27fc1e522a1e50
Berkant Karaca <bkaraca.71m@gmail.com>
2025-01-13 21:07:03 +0300

    EntityState Komutları, Generic Repository Sınıfı ve Ef Sınıfları uygulandı

--- CSharpEgitimKampi301.DataAccessLayer/Abstract/IGenericDal.cs
+++ CSharpEgitimKampi301.DataAccessLayer/Abstract/IGenericDal.cs
@@ -8,10 +8,10 @@ namespace CSharpEgitimKampi301.DataAccessLayer.Abstract
 {
     public interface IGenericDal<T>  where T : class
     {
-        List<T> GetAll();
-        T GetById(int id);
         void Insert(T entity);
         void Update(T entity);
-        void Delete(int id);
+        void Delete(T entity);
+        List<T> GetAll();
+        T GetById(int id);
     }
 }
